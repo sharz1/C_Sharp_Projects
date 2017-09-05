639bd3dc0bf0d1757bbbd287fd25d163c050171b
Sharlee Bryan <sharlee.bryan@gmail.com>
2017-09-05 12:19:03 -0700

    Adding Simple Calculator web app

---
+++ ChallengeSimpleCalculator/ChallengeSimpleCalculator/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeSimpleCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Helvetica;
+        }
+        </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Sharlee&#39;s Simple Calculator</h1>
+            <br />
+            <span class="auto-style1">First Value</span>
+            <asp:TextBox ID="inputBox1" runat="server"></asp:TextBox>
+            <br />
+            <span class="auto-style1">Second Value</span>
+            <asp:TextBox ID="inputBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="plusButton" runat="server" Text="+" OnClick="plusButton_Click" />
+            &nbsp;
+            <asp:Button ID="minusButton" runat="server" Text="-" OnClick="minusButton_Click" />
+            &nbsp;
+            <asp:Button ID="multButton" runat="server" Text="*" OnClick="multButton_Click" />
+            &nbsp;
+            <asp:Button ID="divideButton" runat="server" Text="/" OnClick="divideButton_Click" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server" BackColor="#33CCFF"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeSimpleCalculator/ChallengeSimpleCalculator/Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void plusButton_Click(object sender, EventArgs e)
+        {
+            decimal firstValue = decimal.Parse(inputBox1.Text);
+            decimal secondValue = decimal.Parse(inputBox2.Text);
+            decimal result = (firstValue + secondValue);
+
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void minusButton_Click(object sender, EventArgs e)
+        {
+            decimal firstValue = decimal.Parse(inputBox1.Text);
+            decimal secondValue = decimal.Parse(inputBox2.Text);
+            decimal result = (firstValue - secondValue);
+
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void multButton_Click(object sender, EventArgs e)
+        {
+            decimal firstValue = decimal.Parse(inputBox1.Text);
+            decimal secondValue = decimal.Parse(inputBox2.Text);
+            decimal result;
+            checked
+            {
+                result = (firstValue * secondValue);
+            }
+
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void divideButton_Click(object sender, EventArgs e)
+        {
+            decimal firstValue = decimal.Parse(inputBox1.Text);
+            decimal secondValue = decimal.Parse(inputBox2.Text);
+            decimal result;
+            checked
+            {
+                result = (firstValue / secondValue);
+            }
+
+            resultLabel.Text = result.ToString();
+        }
+    }
+}
