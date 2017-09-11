62aef2ee3467ef6b3c86edc8b3f98a001606b414
Sharlee Bryan <sharlee.bryan@gmail.com>
2017-09-11 13:29:06 -0700

    Adding Spy Performance Tracker app

---
+++ EpicSpiesAssetTracker/EpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>
+                <asp:Image ID="Image1" runat="server" CssClass="auto-style1" Height="150px" ImageUrl="epic-spies-logo.jpg" />
+                <br class="auto-style1" />
+                <br class="auto-style1" />
+                <span class="auto-style1">Asset Performance Tracker</span></h3>
+            <p>
+                Asset Name:
+                <asp:TextBox ID="assetNameTextBox" runat="server" Height="16px"></asp:TextBox>
+            </p>
+            <p>
+                Elections Rigged:
+                <asp:TextBox ID="electionsTextBox" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                Acts of Subterfuge Performed:
+                <asp:TextBox ID="subterfugeTextBox" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Button ID="addassetButton" runat="server" OnClick="addassetButton_Click" Text="Add Asset" />
+            </p>
+            <p>
+                <asp:Label ID="assetdataLabel" runat="server"></asp:Label>
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ EpicSpiesAssetTracker/EpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        //initialize arrays (scoped for Page_Load and Button_Click event use)
+        string[] assetsArray;
+        int[] electionsArray;
+        int[] subterfugeArray;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //If this is the first time loading page, create empty arrays, add to ViewState
+                assetsArray = new string[0];
+                electionsArray = new int[0];
+                subterfugeArray = new int[0];
+
+                ViewState.Add("Assets", assetsArray);
+                ViewState.Add("Elections", electionsArray);
+                ViewState.Add("Subterfuge", subterfugeArray);
+            }
+        }
+
+        protected void addassetButton_Click(object sender, EventArgs e)
+        {
+            //Get arrays from ViewState (previously appended arrays)
+            assetsArray = (string[])ViewState["Assets"];
+            electionsArray = (int[])ViewState["Elections"];
+            subterfugeArray = (int[])ViewState["Subterfuge"];
+
+            //resize arrays so capacity is 1 more for each
+            Array.Resize(ref assetsArray, assetsArray.Length + 1);
+            Array.Resize(ref electionsArray, electionsArray.Length + 1);
+            Array.Resize(ref subterfugeArray, subterfugeArray.Length + 1);
+
+            //find highest index to use for appending new values
+            int high_index = assetsArray.GetUpperBound(0);
+
+            //set new values from form TextBoxes as final indexed values in arrays
+            assetsArray[high_index] = assetNameTextBox.Text;
+            electionsArray[high_index] = int.Parse(electionsTextBox.Text);
+            subterfugeArray[high_index] = int.Parse(subterfugeTextBox.Text);
+
+            //save newly appended arrays back to ViewStates
+            ViewState["Assets"] = assetsArray;
+            ViewState["Elections"] = electionsArray;
+            ViewState["Subterfuge"] = subterfugeArray;
+
+            //get aggregate values for Label output
+            string last_added = assetsArray[high_index];
+            int total_elections = electionsArray.Sum();
+            double ave_subs = subterfugeArray.Average();
+
+            //Output values to Label
+            assetdataLabel.Text = String.Format("Total Elections Rigged: {0}<br />" +
+                "Average Acts of Subterfuge per Asset {1:N2}<br />(Last Asset You Added: {2}", 
+                total_elections, ave_subs, last_added);
+
+        }
+    }
+}
